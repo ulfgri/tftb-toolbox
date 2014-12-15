4b3126f7dee6659b4bfab1e287a385ad531d78ff
Ulf GRIESMANN <ulfgri@gmail.com>
2014-12-14 21:55:43 -0500

    update tf_version.m, ChangeLog

--- tf_version.m
+++ tf_version.m
@@ -17,7 +17,7 @@ function [tb_ver, sw_ver] = tf_version;
 
 % toolbox version and version date
 tb_version = '70+ (beta)';
-tb_date = '2014-December-07';
+tb_date = '2014-December-14';
 ltb_ver = [tb_version, '  (', tb_date, ')'];
 if exist('OCTAVE_VERSION')
     interpreter = 'Octave';
