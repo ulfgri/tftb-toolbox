3b937bc9f8732b3255e09f7d7ef0e4b87c27216b
Ulf GRIESMANN <ulfgri@gmail.com>
2015-04-28 18:56:44 -0400

    28apr2015 - better help text for 'tf_readnk' function.

--- tf_readnk.m
+++ tf_readnk.m
@@ -2,14 +2,17 @@ function [ri] = tf_readnk(material, collection);
 %function [ri] = tf_readnk(material, collection);
 %
 % tf_readnk :  read refractive index data and return them
-%              as a function of wavelength in micrometer
+%              in tabular form as a function of wavelength 
+%              in micrometer
 %
 % Input:
 % material :    name of the material; must correspond to a
 %               file with name '<material>' and extension .nk 
 %               in one of the material collections. E.g. 'sio2' 
 % collection :  name of a material collection, a subdirectory of 
-%               database root directory.
+%               the database root directory. The function
+%               'tf_listnk' can be used to list available
+%               material collections and the materials in them.
 %
 % Output:
 % ri :          a structure with the refractive index sampled
@@ -24,17 +27,17 @@ function [ri] = tf_readnk(material, collection);
 
 % Initial version, Ulf Griesmann, February 2013
 
-% check arguments
-if nargin < 2
-   error('tf_readnk :  must have two arguments.');
+    % check arguments
+    if nargin < 2
+        error('tf_readnk :  must have two arguments.');
+    end
+
+    % collection specific read function for nk data
+    read_nk_func = str2func(['read_', collection]);
+
+    % call read function
+    nk_name = [tf_rootdir(),'nk/',collection,'/',material,'.nk'];
+    [ri.nk, ri.lambda] = read_nk_func(nk_name);
+    ri.name = sprintf('%s (%s)', material, collection);
+    
 end
-
-% collection specific read function for nk data
-read_nk_func = str2func(['read_', collection]);
-
-% call read function
-nk_name = [tf_rootdir(),'nk/',collection,'/',material,'.nk'];
-[ri.nk, ri.lambda] = read_nk_func(nk_name);
-ri.name = sprintf('%s (%s)', material, collection);
-
-return
